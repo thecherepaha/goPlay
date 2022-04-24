25bb4bf286ab69e909c9da7d85ed3afca8f8c1f9
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-24 22:27:40 +0600

    Update login.php

--- views/pages/login.php
+++ views/pages/login.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <div class="navbar-nav">
                     <a href="#" class="nav-item nav-link">Home</a>
-                   
+
                 </div>
                 <div class="navbar-nav ms-auto">
                     <a href="#" class="nav-item nav-link active">Login</a>
@@ -30,6 +30,26 @@
         </div>
     </nav>
 
+    <div class="container">
+        <h2>Sign In</h2>
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 
 </body>
+
 </html>
