301091308dc36e2d95afa3263ffb08007ec7b91e
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-25 01:30:13 +0600

    Update routes.php

--- router/routes.php
+++ router/routes.php
@@ -8,6 +8,6 @@ use App\Controller\Auth;
 Router::page('/','home');
 Router::page('/register','register');
 Router::page('/login','login');
-Router::post('/auth/register', Auth::class, 'register');
+Router::post('./auth/register', Auth::class, 'register');
 
 Router::enable();
