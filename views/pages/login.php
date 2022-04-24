8513bbb5abe3a4ead12e98acfce7a4b0df442255
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-24 22:21:05 +0600

    Update login.php

--- views/pages/login.php
+++ views/pages/login.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Medcare</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/bootstrap.min.js"></script>
 </head>
 
 <body>
@@ -30,5 +31,4 @@
         </div>
     </nav>
 </body>
-
 </html>
