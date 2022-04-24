68744cc9fd77b5c4a5a732f589f8c385148931be
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-24 22:16:16 +0600

    test

--- index.php
+++ index.php
@@ -2,5 +2,3 @@
 
 require_once __DIR__."/vendor/autoload.php";
 require_once __DIR__."/router/routes.php";
-
-// \App\Services\Router::test();
--- views/pages/login.php
+++ views/pages/login.php
@@ -1,69 +1,37 @@
-<!doctype html>
-<html>
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <title>Medcare</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="font-sans antialiased" id="app">
-        <nav class="flex items-center justify-between flex-wrap bg-teal p-6">
-            <div class="flex items-center flex-no-shrink text-white mr-6">
-                <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-                </svg>
-                <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
-            </div>
-            <div class="block sm:hidden">
-                <button @click="toggle"
-                    class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
-                    <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <title>Menu</title>
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-                    </svg>
-                </button>
-            </div>
-            <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-                <div class="text-sm sm:flex-grow">
-                    <a href="#responsive-header"
-                        class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mr-4">
-                        Docs
-                    </a>
-                    <a href="#responsive-header"
-                        class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white mr-4">
-                        Examples
-                    </a>
-                    <a href="#responsive-header"
-                        class="no-underline block mt-4 sm:inline-block sm:mt-0 text-teal-lighter hover:text-white">
-                        Blog
-                    </a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">
+                <img src="assets/logo4.svg" height="28" alt="">
+            </a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="#" class="nav-item nav-link active">Home</a>
+                    <a href="#" class="nav-item nav-link">Profile</a>
+                    <a href="#" class="nav-item nav-link">Messages</a>
                 </div>
-                <div>
-                    <a href="#"
-                        class="no-underline inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:bg-white mt-4 sm:mt-0">Download</a>
+                <div class="navbar-nav ms-auto">
+                    <a href="#" class="nav-item nav-link">Register</a>
+                </div>
+                <div class="navbar-nav ms-auto">
+                    <a href="#" class="nav-item nav-link">Login</a>
                 </div>
             </div>
-        </nav>
-    </div>
-
+        </div>
+    </nav>
 </body>
-<script>
-new Vue({
-    el: '#app',
-    data: {
-        open: false,
-    },
-    methods: {
-        toggle() {
-            this.open = !this.open
-        }
-    }
-})
-</script>
 
 </html>
