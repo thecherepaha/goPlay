5d6f7844e26d1a06ffd4c600d4f5b1e15d7e61dd
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-24 23:00:16 +0600

    Update register.php

--- views/pages/register.php
+++ views/pages/register.php
@@ -17,17 +17,28 @@
         <h2>Sign Up</h2>
         <form>
             <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                <label for="email" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="email">
             </div>
             <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="mb-3">
+                <label for="fullname" class="form-label">Fullname</label>
+                <input type="text" class="form-control" id="fullname">
+            </div>
+            <div class="mb-3">
+                <label for="Avatar" class="form-label">Avatar</label>
+                <input type="file" class="form-control" id="Avatar">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <div class="mb-3">
+                <label for="pass_confirm" class="form-label">Password Confirmation</label>
+                <input type="password" class="form-control" id="pass_confirm">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
