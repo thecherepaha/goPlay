dba07a163d5003545a8bbed5eb2b2ee9d08df291
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-25 00:14:16 +0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     "enable" => true,
     "host" => "srv-pleskdb27.ps.kz",
-    "port" => 3303,
+    "port" => 3306,
     "username" => "medcare1_beta",
     "password" => "Qwerty_1!",
     "db" => "medcare1_beta"
