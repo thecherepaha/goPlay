bc78a6585406c2996ef19ce7d5f0077d01ad80d0
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-25 03:42:45 +0600

    Update head.php

--- views/components/head.php
+++ views/components/head.php
@@ -3,6 +3,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="..\assets\css\bootstrap.min.css">
-    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
     <title>Medcare</title>
 </head>
