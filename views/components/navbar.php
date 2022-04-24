fc4806bb29e3e96c91c93c5f4a60d2173d4009ce
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-25 03:40:32 +0600

    Update navbar.php

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
         <a href="#" class="navbar-brand">
-            <img src="assets/images/logo4.svg" height="50" alt="">
+            <img src="../assets/images/logo4.svg" height="50" alt="">
         </a>
         <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
