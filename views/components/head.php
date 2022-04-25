3e788243696e49e2f60ee2a7af7d45857856203f
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-25 18:01:30 +0600

    test

--- views/components/head.php
+++ views/components/head.php
@@ -4,5 +4,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <script src="../assets/js/bootstrap.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Raleway&family=Roboto:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/scss/custom.css">
     <title>Medcare</title>
 </head>
