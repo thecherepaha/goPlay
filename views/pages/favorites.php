cbbc849366046ea04219abf4147e6b3b60cb35fc
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-27 01:30:17 +0600

    Update favorites.php

--- views/pages/favorites.php
+++ views/pages/favorites.php
@@ -32,7 +32,7 @@
             <div class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-heading<?= $article->id ?>">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapse<?= $article->id ?>" aria-expanded="true"
+                        data-bs-target="#panelsStayOpen-collapse<?= $article->id ?>" aria-expanded="false"
                         aria-controls="panelsStayOpen-collapse<?= $article->id ?>"
                         style="display:inline-block !important;">
                         <div style="float:left;"><strong><?= $article->article_header ?></strong></div>
