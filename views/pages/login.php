8ef8a22c8d6f42734d9bce324aeccf69f4e76bc9
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-26 15:48:33 +0600

    Update login.php

--- views/pages/login.php
+++ views/pages/login.php
@@ -27,10 +27,7 @@
                 <label for="password" class="form-label">Password</label>
                 <input type="password" name="password" class="form-control" id="password">
             </div>
-            <!-- <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div> -->
+            
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
