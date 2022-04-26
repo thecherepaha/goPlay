ca3829a877a193c768faa3628101b454a9f70a72
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-26 15:47:15 +0600

    test

--- views/components/head.php
+++ views/components/head.php
@@ -2,10 +2,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="robots" content="all,follow">
-    <meta name="googlebot" content="index,follow,snippet,archive">
-
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <script src="../assets/js/bootstrap.min.js"></script>
-    <title><?php echo $page_title ?> | Medcare</title>
+    <title><?php echo $page_title; ?> | Medcare</title>
 </head>
