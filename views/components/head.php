3c49b08a9954bc98083e05959831bbdfe5237048
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-26 15:41:02 +0600

    Update head.php

--- views/components/head.php
+++ views/components/head.php
@@ -2,6 +2,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="all,follow">
+    <meta name="googlebot" content="index,follow,snippet,archive">
+    
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <script src="../assets/js/bootstrap.min.js"></script>
     <title><?php if(isset($page_title)) echo $page_title ?> | Medcare</title>
