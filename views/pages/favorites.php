1933c1aa8158f749561d8abf554d86e1d5a1207a
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-27 00:22:22 +0600

    Update favorites.php

--- views/pages/favorites.php
+++ views/pages/favorites.php
@@ -5,6 +5,7 @@
     if($_SESSION["user"]){
         $favorites = $_SESSION["user"]["favorites"];
         $article = \R::findOne('articles', $favorites);
+        print_r($article);
     }else{
         \App\Services\Router::redirect('/login');
     }
