1b426e8ad06cee5865136fa51b3fc672b26310f7
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-26 16:11:49 +0600

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -34,7 +34,7 @@
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#panelsStayOpen-collapse<?= $article->id ?>" aria-expanded="false"
                         aria-controls="panelsStayOpen-collapse<?= $article->id ?>">
-                        <strong><?= $article->article_header ?></strong>     <a href="#"><code><?= $article->article_author ?></code></a>
+                        <strong><?= $article->article_header ?></strong> <a href="#" class="float-right"><code><?= $article->article_author ?></code></a>
                     </button>
                 </h2>
                 <div id="panelsStayOpen-collapse<?= $article->id ?>" class="accordion-collapse collapse"
