ac47513e38ae64813d098a37b75d995eb9312230
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-26 18:10:22 +0600

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -38,7 +38,7 @@
                         style="display:inline-block !important;">
                         <div style="text-align:center;"><strong><?= $article->article_header ?></strong></div>
                         <form action="/auth/addhub" method="post">
-                            <div hidden>
+                            <div>
                                 <input type="text" value = "'. <?= $article->article_id ?> .'" id="article_id" name="article_id">
                             </div>
                             <button type="submit" class="btn btn-primary">+</button>
