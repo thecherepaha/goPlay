c184228d4283353beef0eca24998f301d23e2f30
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-27 17:41:38 +0600

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -28,12 +28,13 @@
         <div class="accordion" id="accordionPanelsStayOpenExample">
             <?php 
                 foreach($categories as $category){
+                    print_r($category);
                     ?>
             <div class="accordion-item">
                 <h2 class="accordion-header" id="heading">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        <?= $category ?>
+                        <?= $category["article_category"] ?>
                     </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
