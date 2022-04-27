c6a01d92aa0a12a1f33b454e6de5717af7472973
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-27 17:01:24 +0600

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -9,6 +9,11 @@
         $articles = \R::find( 'articles');
     }
 
+    $categories = \R::$f->begin()
+    ->addSQL(' SELECT DISTINCT article_category ') 
+    ->from('articles');
+
+    print_r($categories);
     
 ?>
 
@@ -25,10 +30,30 @@
         Page::part('navbar');
     ?>
     <div class="container mt-3">
+
         <div class="accordion" id="accordionPanelsStayOpenExample">
             <?php
                 foreach($articles as $article){
                     ?>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="heading">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Accordion Item #1
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
+                        collapse plugin adds the appropriate classes that we use to style each element. These classes
+                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
+                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
+                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
+                        does limit overflow.
+                    </div>
+                </div>
+            </div>
             <div class="accordion-item">
                 <h2 class="accordion-header" id="panelsStayOpen-heading<?= $article->id ?>">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
@@ -43,7 +68,8 @@
                             </div>
 
                             <button type="submit" class="btn <?= Page::class($article->id); ?>">
-                                Participants <span class="badge bg-secondary"><?= Page::likes($article->id, $article->article_author); ?></span>
+                                Participants <span
+                                    class="badge bg-secondary"><?= Page::likes($article->id, $article->article_author); ?></span>
                             </button>
 
                         </form>
@@ -63,7 +89,7 @@
                                     if($article->article_author !== $participant->fullname){
 
                                     ?>
-                                    <a href="#" class="list-group-item list-group-item-action"><?= $participant->fullname ?></a>
+                            <a href="#" class="list-group-item list-group-item-action"><?= $participant->fullname ?></a>
                             <?php
                                     }
                                 }
