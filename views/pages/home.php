e972676eb2fdc701985ee9d6423223ea51dc1252
Beknazar Serikbayev <beknazarserikbaev@gmail.com>
2022-04-27 17:34:07 +0600

    Update home.php

--- views/pages/home.php
+++ views/pages/home.php
@@ -23,77 +23,84 @@
     <?php
         Page::part('navbar');
     ?>
-    <?php var_dump($categories) ?>;
     <div class="container mt-3">
 
         <div class="accordion" id="accordionPanelsStayOpenExample">
-            <?php
-                foreach($articles as $article){
+            <?php 
+                foreach($categories as $category){
                     ?>
-            <!-- <div class="accordion-item">
+            <div class="accordion-item">
                 <h2 class="accordion-header" id="heading">
                     <button class="accordion-button" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                        Accordion Item #1
+                        <?= $category->article_category ?>
                     </button>
                 </h2>
                 <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
                     data-bs-parent="#accordionExample">
                     <div class="accordion-body">
-                        <strong>This is the first item's accordion body.</strong> It is shown by default, until the
-                        collapse plugin adds the appropriate classes that we use to style each element. These classes
-                        control the overall appearance, as well as the showing and hiding via CSS transitions. You can
-                        modify any of this with custom CSS or overriding our default variables. It's also worth noting
-                        that just about any HTML can go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
-                    </div>
-                </div>
-            </div> -->
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-heading<?= $article->id ?>">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#panelsStayOpen-collapse<?= $article->id ?>" aria-expanded="false"
-                        aria-controls="panelsStayOpen-collapse<?= $article->id ?>"
-                        style="display:inline-block !important;">
-                        <div style="text-align:center;"><strong><?= $article->article_header ?></strong></div>
-                        <form action="/auth/addhub" method="post">
-                            <div hidden>
-                                <input type="text" value="<?= $article->id ?>" id="article_id" name="article_id" />
-                                <input type="text" value="<?= $_SESSION["user"]["id"] ?>" id="user_id" name="user_id" />
-                            </div>
+                        <?php foreach($articles as $article){
+                            if($article->article_category === $category->article_category){
+                        ?>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="panelsStayOpen-heading<?= $article->id ?>">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#panelsStayOpen-collapse<?= $article->id ?>" aria-expanded="false"
+                                    aria-controls="panelsStayOpen-collapse<?= $article->id ?>"
+                                    style="display:inline-block !important;">
+                                    <div style="text-align:center;"><strong><?= $article->article_header ?></strong>
+                                    </div>
+                                    <form action="/auth/addhub" method="post">
+                                        <div hidden>
+                                            <input type="text" value="<?= $article->id ?>" id="article_id"
+                                                name="article_id" />
+                                            <input type="text" value="<?= $_SESSION["user"]["id"] ?>" id="user_id"
+                                                name="user_id" />
+                                        </div>
 
-                            <button type="submit" class="btn <?= Page::class($article->id); ?>">
-                                Participants <span
-                                    class="badge bg-secondary"><?= Page::likes($article->id, $article->article_author); ?></span>
-                            </button>
+                                        <button type="submit" class="btn <?= Page::class($article->id); ?>">
+                                            Participants <span
+                                                class="badge bg-secondary"><?= Page::likes($article->id, $article->article_author); ?></span>
+                                        </button>
 
-                        </form>
-                        <div style="float:right;">Author: <code><?= $article->article_author ?></code></div>
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapse<?= $article->id ?>" class="accordion-collapse collapse"
-                    aria-labelledby="panelsStayOpen-heading<?= $article->id ?>">
-                    <div class="accordion-body">
+                                    </form>
+                                    <div style="float:right;">Author: <code><?= $article->article_author ?></code></div>
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapse<?= $article->id ?>" class="accordion-collapse collapse"
+                                aria-labelledby="panelsStayOpen-heading<?= $article->id ?>">
+                                <div class="accordion-body">
 
-                        <div class="list-group">
-                            <a href="#" class="list-group-item list-group-item-action active">
-                                <?= $article->article_author ?>
-                            </a>
-                            <?php $participants = \R::find('users',' favorites = ?', [$article->id]); 
+                                    <div class="list-group">
+                                        <a href="#" class="list-group-item list-group-item-action active">
+                                            <?= $article->article_author ?>
+                                        </a>
+                                        <?php $participants = \R::find('users',' favorites = ?', [$article->id]); 
                                 foreach($participants as $participant){
                                     if($article->article_author !== $participant->fullname){
 
                                     ?>
-                            <a href="#" class="list-group-item list-group-item-action"><?= $participant->fullname ?></a>
-                            <?php
+                                        <a href="#"
+                                            class="list-group-item list-group-item-action"><?= $participant->fullname ?></a>
+                                        <?php
                                     }
                                 }
                                 ?>
 
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
+
+                        <?php }
+
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
+
             <?php
                 }
             ?>
